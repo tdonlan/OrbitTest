2528ed3008aec9bc40c8ebc190584a3a84eac00f
TIM DONLAN <TDONLAN@nextgen.com>
2015-09-16 12:46:10 -0400

    Initial Commit

---
+++ Assets/GameControllerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+
+    public Rigidbody2D moonRigidBody;
+    private float keyForce = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateControl();
+	}
+
+    private void UpdateControl()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moonRigidBody.AddForce(new Vector2(-keyForce, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moonRigidBody.AddForce(new Vector2(keyForce, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moonRigidBody.AddForce(new Vector2(0, -keyForce));
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moonRigidBody.AddForce(new Vector2(0, keyForce));
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+        {
+            Camera.main.orthographicSize++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+        {
+            Camera.main.orthographicSize--;
+        }
+    }
+}
---
+++ Assets/PlanetScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlanetScript : MonoBehaviour {
+
+    List<Rigidbody2D> rigidBodyList;
+    Rigidbody2D planetRigidBody;
+
+    //private double G = 6.67 * (Mathf.Pow(10,-11));
+    private double G = .05;
+    
+
+	// Use this for initialization
+	void Start () {
+        init();
+	}
+
+    private void init()
+    {
+        this.planetRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        var rbArray = GameObject.FindObjectsOfType<Rigidbody2D>();
+        rigidBodyList = new List<Rigidbody2D>(rbArray);
+        rigidBodyList.Remove(planetRigidBody);
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateGravity();
+	}
+
+
+
+    //send out gravity force to everything in vicinity
+    private void UpdateGravity()
+    {
+        foreach (var body in rigidBodyList)
+        {
+            Vector2 direction = this.planetRigidBody.position - body.position;
+            direction.Normalize();
+            float gravityForce = getGravityForce(planetRigidBody, body);
+            direction = new Vector2(direction.x * gravityForce, direction.y * gravityForce);
+
+            body.AddForce(direction);
+            Debug.Log(string.Format("{0}, {1}", direction.x, direction.y));
+        }
+
+    }
+
+    private float getGravityForce(Rigidbody2D body1, Rigidbody2D body2)
+    {
+        var dist = Vector2.Distance(body1.position, body2.position);
+        double force = G * ((body1.mass * body2.mass) / (dist * dist));
+        return (float)force;
+    }
+}
